7b9ecacc0f235e57cd6a8bd2fbdbb863bb4fcc47
Prakhar Verma <prakhar.verma@hashedin.com>
2020-09-07 21:18:03 +0530

    login controller

--- src/main/java/com/example/blackninja/api/UserAuthController.java
+++ src/main/java/com/example/blackninja/api/UserAuthController.java
@@ -1,11 +1,13 @@
 package com.example.blackninja.api;
 
+import com.example.blackninja.dtos.request.LoginRequest;
 import com.example.blackninja.dtos.request.UserRequest;
 import com.example.blackninja.dtos.response.GenericResponse;
 import com.example.blackninja.dtos.response.UserRegistrationResponse;
 import com.example.blackninja.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -15,15 +17,29 @@ import java.util.Collections;
 @RequestMapping("/api/user")
 public class UserAuthController {
 
-    @Autowired
     private UserService userService;
+
+    @Autowired
+    public UserAuthController(UserService userService) {
+        this.userService = userService;
+    }
 
     @PostMapping
     public ResponseEntity<GenericResponse<UserRegistrationResponse>> registerUser(@RequestBody UserRequest userRequest) {
-        UserRegistrationResponse response = userService.registerUser(userRequest);
+        UserRegistrationResponse response = userService.userRegistration(userRequest);
         GenericResponse<UserRegistrationResponse> genericResponse = new GenericResponse(
-                response, "USER REGISTRATION",
-                HttpStatus.CREATED, HttpStatus.CREATED.value(), Collections.emptyList());
-        return new ResponseEntity<GenericResponse<UserRegistrationResponse>>(genericResponse, HttpStatus.CREATED);
+                response,
+                "USER REGISTRATION",
+                HttpStatus.CREATED,
+                HttpStatus.CREATED.value(),
+                Collections.emptyList()
+        );
+        return new ResponseEntity(genericResponse, HttpStatus.CREATED);
+    }
+
+    @PostMapping(value = "/login", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<String> login(@RequestBody LoginRequest loginRequest) {
+        String response = userService.login(loginRequest);
+        return new ResponseEntity(response, HttpStatus.OK);
     }
 }
