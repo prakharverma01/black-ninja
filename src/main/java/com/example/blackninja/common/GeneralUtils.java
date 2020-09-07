a8b43b787ad84320cd25268e3828a51388f18516
Prakhar Verma <prakhar.verma@hashedin.com>
2020-09-07 21:13:27 +0530

    added util method to get full name

--- src/main/java/com/example/blackninja/common/GeneralUtils.java
+++ src/main/java/com/example/blackninja/common/GeneralUtils.java
@@ -1,13 +1,23 @@
 package com.example.blackninja.common;
 
+import org.apache.commons.lang3.StringUtils;
 import org.apache.commons.validator.routines.EmailValidator;
-
-import java.util.regex.Pattern;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class GeneralUtils {
 
     public static boolean verifyEmailString(String email) {
         EmailValidator validator = EmailValidator.getInstance();
         return validator.isValid(email);
+    }
+
+    public static String getFullName(String firstName, String lastName) {
+        List<String> nameList = Arrays.asList(firstName, lastName).stream()
+                .filter(StringUtils::isNotBlank)
+                .filter(StringUtils::isNotEmpty)
+                .collect(Collectors.toList());
+        return String.join(" ", nameList);
     }
 }
