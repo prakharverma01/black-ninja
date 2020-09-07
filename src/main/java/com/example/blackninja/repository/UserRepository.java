e9a73e5635e61c7e3c6619f0a0e9ed4fdd0ca4ef
Prakhar Verma <prakhar.verma@hashedin.com>
2020-09-07 21:18:16 +0530

    new methods in user repo

--- src/main/java/com/example/blackninja/repository/UserRepository.java
+++ src/main/java/com/example/blackninja/repository/UserRepository.java
@@ -2,6 +2,8 @@ package com.example.blackninja.repository;
 
 import com.example.blackninja.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
@@ -9,5 +11,18 @@ import java.util.Optional;
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 
-    public Optional<User> findByEmail(String email);
+    Optional<User> findByEmail(String email);
+
+    @Query(value = " select u.* from users u, user_company_entitlement uce, companies c " +
+        " where u.id = uce.user_id " +
+        " and c.id = uce.company_id " +
+        " and uce.role = :role " +
+        " and c.company_name = :companyName " +
+        " and u.email = :email "
+    , nativeQuery = true)
+    Optional<User> findByEmailAndCompanyAndRole(
+    @Param("email") String email,
+    @Param("companyName") String companyName,
+    @Param("role") String role);
+
 }
