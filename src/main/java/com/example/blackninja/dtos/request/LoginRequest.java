b20cd59079ce28dc55c5afece8f589283d9e455b
Prakhar Verma <prakhar.verma@hashedin.com>
2020-09-07 21:14:02 +0530

    loginRequest DTO

--- src/main/java/com/example/blackninja/dtos/request/LoginRequest.java
+++ src/main/java/com/example/blackninja/dtos/request/LoginRequest.java
@@ -4,6 +4,16 @@ public class LoginRequest {
     private String email;
     private String password;
     private String companyName;
+    private String role;
+
+    public LoginRequest(String email, String password, String companyName, String role) {
+        this.email = email;
+        this.password = password;
+        this.companyName = companyName;
+        this.role = role;
+    }
+
+    public String getRole() { return role; }
 
     public String getEmail() {
         return email;
