5e0f9d409bcf9512132b4c755cf2ac5c1e727418
Prakhar Verma <prakhar.verma@hashedin.com>
2020-10-18 19:46:51 +0530

    changes in security config

--- src/main/java/com/example/blackninja/security/SecurityConfig.java
+++ src/main/java/com/example/blackninja/security/SecurityConfig.java
@@ -6,10 +6,6 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.stereotype.Component;
-import javax.servlet.*;
-import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
 
 @Order(1)
 @Configuration
@@ -23,18 +19,5 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                     .and().authorizeRequests().anyRequest().permitAll();
 
         } catch (Exception ex) {}
-    }
-}
-
-@Component
-class CorsFilter implements Filter {
-    @Override
-    public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
-        HttpServletResponse response = (HttpServletResponse) res;
-        response.setHeader("Access-Control-Allow-Origin", "*");
-        response.setHeader("Access-Control-Allow-Methods", "POST, PUT, GET, OPTIONS, DELETE");
-        response.setHeader("Access-Control-Max-Age", "3600");
-        response.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization, Content-Length, X-Requested-With");
-        chain.doFilter(req, res);
     }
 }
